d31f51f14bba47caa8d90053b986be5a1634243b
demansn <demansn@gmail.com>
2017-02-07 08:15:12 +0200

    Запуситл devtool

--- chrome-devtool/src/panel.js
+++ chrome-devtool/src/panel.js
@@ -1,6 +1,2 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-var node = document.getElementById('root');
-var Panel = require('../../src/Panel.jsx');
 
-ReactDOM.render(<Panel />, node);
+require('../../src/App.jsx');
--- config/webpack.config.js
+++ config/webpack.config.js
@@ -10,7 +10,12 @@ module.exports = {
                 test: /\.jsx?/,
                 exclude: /node_modules/,
                 loader: 'babel-loader'
-            }
+            },
+			{
+				test: /\.css?/,
+                exclude: /node_modules/,
+                loaders: ['style-loader', 'css-loader']
+			}
         ]
     }
 };
